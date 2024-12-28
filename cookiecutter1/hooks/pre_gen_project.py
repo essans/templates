083d723f4e6061e27b33b75fa07207a7b2d2da98
Essan <essans@me.com>
2024-12-28 00:22:43 -0500

    added hooks and .gitkeep files

---
+++ cookiecutter1/hooks/pre_gen_project.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+import os
+import pathlib
+
+
+def example_function():
+    print("running pre_gen example_function")
+
+
+if __name__ == "__main__":
+    example_function()
+
+
